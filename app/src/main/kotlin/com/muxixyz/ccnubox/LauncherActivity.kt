ecffe6ca8c162776f538b286d6f6b0a58bfbe1ae
钟伊凡 <44168988+zyflool@users.noreply.github.com>
2021-03-17 11:35:57 +0800

    modify module architecture (#8)
    * modify module architecture

--- app/src/main/kotlin/com/muxixyz/ccnubox/LauncherActivity.kt
+++ app/src/main/kotlin/com/muxixyz/ccnubox/LauncherActivity.kt
@@ -4,9 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
-import com.muxixyz.ccnubox.home.ui.HomeActivity
-//import me.xx2bab.bro.annotations.BroActivity
-//import me.xx2bab.bro.core.Bro
+import com.muxixyz.ccnubox.main.ui.main.MainActivity
 import net.muxi.huashiapp.R
 
 class LauncherActivity : AppCompatActivity() {
@@ -20,7 +18,7 @@ class LauncherActivity : AppCompatActivity() {
         handler.postDelayed({
 //            Bro.get().startActivityFrom(this@LauncherActivity)
 //                .toUrl("ccnubox://home")
-            startActivity(Intent(this@LauncherActivity, HomeActivity::class.java))
+            startActivity(Intent(this@LauncherActivity, MainActivity::class.java))
         }, 500)
     }
 }
